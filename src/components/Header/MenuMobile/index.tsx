b65cc930e29ff65fc56eef6b6b55576966651c22
gian <lgian584@gmail.com>
2025-05-28 11:17:04 -0300

    fix: update dropdown item links to use dynamic URL

--- src/components/Header/MenuMobile/index.tsx
+++ src/components/Header/MenuMobile/index.tsx
@@ -1,15 +1,17 @@
 import Dropdown from "react-bootstrap/Dropdown";
 import { DropdownContainer } from "./styles";
 
+const URL = import.meta.env.VITE_URL;
+
 export default function MenuMobile() {
   return (
     <DropdownContainer id="dropdown-basic">
       <Dropdown.Toggle>Menu</Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="/">Tendências</Dropdown.Item>
-        <Dropdown.Item href="/list">Minha Lista</Dropdown.Item>
-        <Dropdown.Item href="/search">Pesquisar</Dropdown.Item>
+        <Dropdown.Item href={`${URL}`}>Tendências</Dropdown.Item>
+        <Dropdown.Item href={`${URL}/list`}>Minha Lista</Dropdown.Item>
+        <Dropdown.Item href={`${URL}/search`}>Pesquisar</Dropdown.Item>
       </Dropdown.Menu>
     </DropdownContainer>
   );
