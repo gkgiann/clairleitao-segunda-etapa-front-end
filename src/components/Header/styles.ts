21efd3d681074c52060c731c79928f3281bf5909
gian <lgian584@gmail.com>
2025-05-28 11:03:27 -0300

    style: fix header to top

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,7 +5,14 @@ export const HeaderContainer = styled(Container)`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 3.2rem auto;
+  padding: 2.4rem;
+
+  position: sticky;
+  top: 0;
+  z-index: 50;
+  background-color: rgba(238, 246, 250, 0.8);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
 
   img {
     width: 3.6rem;
